caf897d16c100bd14af759d294dad0ab93055dc8
JotaFan <jotaflame@gmail.com>
2023-06-01 15:47:10 +0200

    Add last missing line, not showing in table

--- coverage_handler.py
+++ coverage_handler.py
@@ -49,6 +49,9 @@ def order_lines_not_covered(lines_not_covered):
                 lines = str(lines_not_covered[i])
             lines_str += f"{lines} "
             lines = []
+    last_missing = str(lines_not_covered[-1])
+    if last_missing not in lines_str:
+        lines_str += f"{last_missing}"
     return lines_str
 
 
