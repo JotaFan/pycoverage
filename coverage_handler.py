5bed51f43176c502bf04242501ec66a56bcd1f6a
Daniel Moreno <dani@photonbit.es>
2023-07-27 14:47:58 +0200

    refactor(handler): trim the main flow into a linear function

--- coverage_handler.py
+++ coverage_handler.py
@@ -11,33 +11,6 @@ COV_THRESHOLD_SINGLE_FAIL = False
 COV_THRESHOLD_TOTAL_FAIL = False
 COV_BADGE_FILE_PATH = os.environ.get("COV_BADGE_FILE_PATH", "docs/badges/coverage.svg")
 
-os.makedirs(os.path.dirname(COV_BADGE_FILE_PATH), exist_ok=True)
-
-coverage_file = Path(COVERAGE_FILE_PATH)
-with coverage_file.open("r") as file:
-    data = json.load(file)
-output = list()
-
-total_coverage = round(data["totals"]["percent_covered"], 2)
-color = "red"
-
-if 50 >= total_coverage > 20:
-    color = "orange"
-elif 70 >= total_coverage > 50:
-    color = "yellow"
-elif 90 >= total_coverage > 70:
-    color = "green"
-elif 100 >= total_coverage > 90:
-    color = "brightgreen"
-
-
-output.append(
-    f"![pycoverage](https://img.shields.io/static/v1?"
-    f"label=pycoverage🛡️&message={total_coverage}%&color={color})"
-)
-output.append("|Name|Stmts|Miss|Cover|Missing|")
-output.append("| ------ | ------ | ------ | ------ |------ |")
-
 
 def sort_lines_not_covered(lines_not_covered: List[int]) -> str:
     if not lines_not_covered:  # handle empty list
@@ -54,32 +27,63 @@ def sort_lines_not_covered(lines_not_covered: List[int]) -> str:
     return lines_str.strip()
 
 
-for file_path, file_data in data.get("files", dict()).items():
-    file_summary = file_data["summary"]
-    num_statements = file_summary["num_statements"]
-    missing_lines = file_summary["missing_lines"]
-    lines_not_covered = file_data["missing_lines"]
-    if isinstance(lines_not_covered, list):
-        lines_not_covered = sort_lines_not_covered(lines_not_covered)
-    percent_covered = round(file_summary["percent_covered"], 2)
-    if percent_covered < COVERAGE_SINGLE_THRESHOLD:
-        COV_THRESHOLD_SINGLE_FAIL = True
+def main():
+    global COV_THRESHOLD_SINGLE_FAIL, COV_THRESHOLD_TOTAL_FAIL
+
+    os.makedirs(os.path.dirname(COV_BADGE_FILE_PATH), exist_ok=True)
+
+    coverage_file = Path(COVERAGE_FILE_PATH)
+    with coverage_file.open("r") as file:
+        data = json.load(file)
+    output = list()
+
+    total_coverage = round(data["totals"]["percent_covered"], 2)
+    color = "red"
+
+    if 50 >= total_coverage > 20:
+        color = "orange"
+    elif 70 >= total_coverage > 50:
+        color = "yellow"
+    elif 90 >= total_coverage > 70:
+        color = "green"
+    elif 100 >= total_coverage > 90:
+        color = "brightgreen"
+
     output.append(
-        f"|{file_path}|{num_statements}|{missing_lines}|{percent_covered}%|{lines_not_covered}"
+        f"![pycoverage](https://img.shields.io/static/v1?"
+        f"label=pycoverage🛡️&message={total_coverage}%&color={color})"
     )
+    output.append("|Name|Stmts|Miss|Cover|Missing|")
+    output.append("| ------ | ------ | ------ | ------ |------ |")
 
-totals = data["totals"]
+    for file_path, file_data in data.get("files", dict()).items():
+        file_summary = file_data["summary"]
+        num_statements = file_summary["num_statements"]
+        missing_lines = file_summary["missing_lines"]
+        lines_not_covered = file_data["missing_lines"]
+        if isinstance(lines_not_covered, list):
+            lines_not_covered = sort_lines_not_covered(lines_not_covered)
+        percent_covered = round(file_summary["percent_covered"], 2)
+        if percent_covered < COVERAGE_SINGLE_THRESHOLD:
+            COV_THRESHOLD_SINGLE_FAIL = True
+        output.append(
+            f"|{file_path}|{num_statements}|{missing_lines}|{percent_covered}%|{lines_not_covered}"
+        )
+
+    totals = data["totals"]
+
+    output.append(f'|TOTAL|{totals["num_statements"]}|{totals["missing_lines"]}|{total_coverage}%|')
+
+    print(*output, sep="\n")
+
+    if round(totals["percent_covered"], 2) < COVERAGE_TOTAL_THRESHOLD:
+        COV_THRESHOLD_TOTAL_FAIL = True
+
+    if COV_THRESHOLD_SINGLE_FAIL:
+        sys.exit(101)
+    if COV_THRESHOLD_TOTAL_FAIL:
+        sys.exit(102)
 
 
-output.append(f'|TOTAL|{totals["num_statements"]}|{totals["missing_lines"]}|{total_coverage}%|')
-
-
-print(*output, sep="\n")
-
-if round(totals["percent_covered"], 2) < COVERAGE_TOTAL_THRESHOLD:
-    COV_THRESHOLD_TOTAL_FAIL = True
-
-if COV_THRESHOLD_SINGLE_FAIL:
-    sys.exit(101)
-if COV_THRESHOLD_TOTAL_FAIL:
-    sys.exit(102)
+if __name__ == "__main__":
+    main()
